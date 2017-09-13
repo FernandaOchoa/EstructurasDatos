31eaa924779c96f66be39fa3cc2b7f78ebfb2fc5
Fernanda Ochoa <monc_sip@hotmail.com>
2017-09-13 13:04:09 -0500

    add: Hannoi Sample

--- src/com/fernandaochoa/Unidad1/TorresHanoi.java
+++ src/com/fernandaochoa/Unidad1/TorresHanoi.java
@@ -4,4 +4,7 @@ package com.fernandaochoa.Unidad1;
  * Created by monsh on 13/09/2017.
  */
 public class TorresHanoi {
+    public static void main(String[] args) {
+        System.out.println("Torres de Hannois");
+    }
 }
